41b97ce2a6d47dbcfbc2fa4dcff1f8493f84949d
foolcodes <evedansh123@gmail.com>
2025-11-10 17:00:01 +0530

    feat: completed with the whole implementation

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use std::sync::{Arc, Mutex};
-use axum::{Router, extract::Request, middleware::{self, Next}, response::IntoResponse, routing::get};
+use std::{sync::{Arc, Mutex}, time::Duration};
+use axum::{Router, extract::Request, http::StatusCode, middleware::{self, Next}, response::IntoResponse, routing::get};
 use chrono::{DateTime, Utc};
 use tokio::{net::TcpListener, signal};
 use uuid::Uuid;
 struct Span {
     span_id: String,
     parent_id: Option<String>,
+    name: String,
     start_time: DateTime<Utc>,
-    end_time: DateTime<Utc>,
+    end_time: Option<DateTime<Utc>>,
     duration_ms: Option<u128>,
 }
-
+#[derive(Clone)]
 struct TraceContext {
     trace_id: String,
     spans: Arc<Mutex<Vec<Span>>>
@@ -21,6 +22,39 @@ impl TraceContext {
         Self {
             trace_id: Uuid::new_v4().to_string(),
             spans: Arc::new(Mutex::new(Vec::new())),
+        }
+    }
+
+    fn start_span(&self, name: impl Into<String>, parent_id: Option<String>) -> String {
+        let id = Uuid::new_v4().to_string();
+        let span = Span {
+            span_id: id.clone(),
+            parent_id,
+            name: name.into(),
+            start_time: Utc::now(),
+            end_time: None,
+            duration_ms: None,
+        };
+
+        let mut g = self.spans.lock().unwrap();
+        g.push(span);
+        id
+    }
+
+    fn end_span(&self, span_id: &str) {
+        let mut g = self.spans.lock().unwrap();
+        if let Some(span) = g.iter_mut().rev().find(|s| s.span_id == span_id) {
+            let end = Utc::now();
+            span.end_time = Some(end);
+            span.duration_ms = span
+            .start_time
+            .signed_duration_since(span.start_time)
+            .to_std()
+            .ok()
+            .map(|_| end.signed_duration_since(span.start_time).num_milliseconds() as u128)
+            .or_else(|| {
+                Some((end.timestamp_millis() - span.start_time.timestamp_millis()) as u128)
+            })
         }
     }
 }
@@ -36,7 +70,7 @@ fn parse_traceparent(header_val: &str) -> Option<(String, String)> {
     }
 }
 
-async fn trace_middleware<B>(mut req: Request<B>, next: Next) -> impl IntoResponse {
+async fn trace_middleware(mut req: Request<axum::body::Body>, next: Next) -> impl IntoResponse {
     let headers = req.headers();
     let (trace_ctx, incoming_parent_id) = if let Some(tp) = headers.get("traceparent") {
         if let Ok(s) = tp.to_str() {
@@ -55,12 +89,56 @@ async fn trace_middleware<B>(mut req: Request<B>, next: Next) -> impl IntoRespon
     } else {
          (TraceContext::new(), None) 
     };
+
+    let root_name = format!(
+        "{} {}",
+        req.method(), 
+        req.uri().path_and_query().map(|p| p.as_str()).unwrap_or("/"));
+
+    let root_span_id = trace_ctx.start_span(root_name, incoming_parent_id);
+    req.extensions_mut().insert(trace_ctx.clone());
+
+    let response = next.run(req).await;
+
+    trace_ctx.end_span(&root_span_id);
+    let spans = trace_ctx.spans.lock().unwrap();
+    println!("------------------------ Trace: {} -----------------------------", trace_ctx.trace_id);
+        for span in spans.iter() {
+            println!(
+                "  Span: {} | Parent: {:?} | Name: {} | Duration: {:?}ms",
+                span.span_id,
+                span.parent_id,
+                span.name,
+                span.duration_ms
+            );
+        }
+    println!("--------------------------------------------------------------\n");
+
+    response
 }
+
+async fn get_user_handler(ctx_ext: Option<axum::extract::Extension<TraceContext>>) -> impl IntoResponse {
+    let ctx = match ctx_ext {
+        Some(axum::extract::Extension(c)) => c,
+        None => {
+            TraceContext::new()
+        }
+    };
+
+    let route_span = ctx.start_span("Users route hit", None);
+    let db_span = ctx.start_span("DB query execution", Some(route_span.clone()));
+
+    ctx.end_span(&db_span);
+    tokio::time::sleep(Duration::from_millis(80)).await;
+    ctx.end_span(&route_span);
+    (StatusCode::OK, "user data");
+}   
+
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(hello))
+        .route("/users/{id}", get(get_user_handler))
         .layer(middleware::from_fn(trace_middleware));
     
     let address = "127.0.0.1:3000";
